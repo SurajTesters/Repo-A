5425a4e57195639699876ae6b68ff98081c92ef6
SurajTesters <deepakkumar7714@outlook.com>
2024-07-05 08:57:21 -0700

    FlipKart Project

---
+++ FlipKart/src/flipKart_URL/LaunchFlipkart.java
@@ -0,0 +1,12 @@
+package flipKart_URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchFlipkart 
+{
+public static void main(String[] args) {
+	LaunchFlipkart ob = new LaunchFlipkart();
+	WebDriver driver = new ChromeDriver();
+}
+}
